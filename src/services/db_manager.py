b686308fb0b3a35c522fe11a4a33b9f22383f402
Dominik <46548557+kiritodom53@users.noreply.github.com>
2022-03-22 12:11:56 +0100

    Úprava db manageru

--- src/services/db_manager.py
+++ src/services/db_manager.py
@@ -5,16 +5,20 @@ import mariadb
 
 class Connection:
     def __init__(self, database=os.getenv("DATABASE"), select_db=True):
-        if database is None:
-            database = os.getenv("DATABASE")
+        db = database
+        if db is None:
+            db = os.getenv("DATABASE")
+        elif db != os.getenv("DATABASE"):
+            db = f"{os.getenv('DB_PREFIX')}{db}"
 
         if select_db:
             self.conn = mariadb.connect(
+
                 user=os.getenv('USER'),
                 password=os.getenv('PASSWORD'),
                 host=os.getenv('HOST'),
                 port=int(os.getenv('PORT')),
-                database=database
+                database=db
             )
         else:
             self.conn = mariadb.connect(
@@ -23,6 +27,7 @@ class Connection:
                 host=os.getenv('HOST'),
                 port=int(os.getenv('PORT'))
             )
+
         self.cur = self.conn.cursor()
         self.conn.autocommit = True
 
